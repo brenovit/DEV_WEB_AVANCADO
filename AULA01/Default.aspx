7c2ff29d0bccde2e444b09f94ceda1a40ceb97b6
BRENO-UJ <tecnologia@TECNOLOGIA-PC>
2017-02-17 21:24:30 -0300

    Pagina que calcula o IMC

---
+++ AULA01/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculo - IMC</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblPeso" runat="server" Text="Peso: "></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPeso" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblAltura" runat="server" Text="Altura: "></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtAltura" runat="server"></asp:TextBox><br />
+                        <br />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnOk" runat="server" Text="IMC" OnClick="btnOk_Click" />
+                    </td>
+                </tr>
+            </table>
+            <p>
+                <asp:Label ID="lblResultado" runat="server" Text="" />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AULA01/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //txtNome.Text = "texto padrão";
+
+        if (!IsPostBack)
+        {            
+            //txtNome.Text = "texto novo";
+        }
+    }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        string mensagem = "";
+        try
+        {
+            double peso = Convert.ToDouble(txtPeso.Text);
+            double altura = Convert.ToDouble(txtAltura.Text);
+            double IMC = peso / (Math.Pow(altura, 2));           
+
+            if (IMC < 17)
+                mensagem = "IMC: " + IMC.ToString("N") + " - Muito abaixo do peso";
+            else if (IMC > 17 && IMC < 18.49)
+                mensagem = "IMC: " + IMC.ToString("N") + " - Abaixo do peso";
+            else if (IMC > 18.5 && IMC < 24.99)
+                mensagem = "IMC: " + IMC.ToString("N") + " - Peso normal";
+            else if (IMC > 25 && IMC < 29.99)
+                mensagem = "IMC: " + IMC.ToString("N") + " - Acima do peso";
+            else if (IMC > 30 && IMC < 34.99)
+                mensagem = "IMC: " + IMC.ToString("N") + " - Obesidade I";
+            else if (IMC > 35 && IMC < 39.99)
+                mensagem = "IMC: " + IMC.ToString("N") + " - Obesidade II (severa)";
+            else
+                mensagem = "IMC: " + IMC.ToString("N") + " - Obesidade III (mórbida)";
+            
+        }
+        catch (Exception ex)
+        {
+            mensagem = "Impossível fazer o calculo. Verifique se os campos estão corretos.";
+        }
+        finally
+        {
+            lblResultado.Text = mensagem;
+        }
+    }
+}
+ 
