bfef38bf50dfa63a1bb9295ccf737079d480537a
tecnologia <tecnologia@TECNOLOGIA-PC>
2017-03-31 19:11:12 -0300

    AULA05-2

---
+++ AULA05-2/App_Code/tbProdutoDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for TbVendaDTO
+/// </summary>
+public class tbProdutoDTO
+{
+
+    public int idProduto { get; set; }
+    public string descricao { get; set; }
+    public double valor { get; set; }
+    public string fornecedor { get; set; }
+
+    public tbProdutoDTO()
+    {
+    }
+
+    public tbProdutoDTO(int idProduto, string descricao, double valor, string fornecedor)
+    {
+        this.idProduto = idProduto;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.fornecedor = fornecedor;
+    }
+
+    
+}
